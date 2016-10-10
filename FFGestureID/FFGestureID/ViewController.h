073c4781efce32135394f6c70a966e83f8321699
张玲玉 <2949820345@qq.com>
2016-10-10 14:52:28 +0800

    提交

---
+++ FFGestureID/FFGestureID/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FFGestureID
+//
+//  Created by 张玲玉 on 16/10/10.
+//  Copyright © 2016年 bj.zly.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FFGestureID/FFGestureID/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  FFGestureID
+//
+//  Created by 张玲玉 on 16/10/10.
+//  Copyright © 2016年 bj.zly.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
